ef5b3854a3504a8cab2f7a84c50c8be031137363
ingvord <mail@ingvord.ru>
2021-10-14 14:33:13 +0200

    Upgrade JTango

--- src/main/java/org/tango/camel/component/TangoComponent.java
+++ src/main/java/org/tango/camel/component/TangoComponent.java
@@ -1,6 +1,6 @@
 package org.tango.camel.component;
 
-import fr.esrf.TangoApi.DeviceProxyFactory;
+import fr.esrf.TangoApi.DeviceProxy;
 import fr.soleil.tango.clientapi.TangoDevice;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Endpoint;
@@ -21,7 +21,7 @@ public class TangoComponent extends DefaultComponent {
     }
 
     protected Endpoint createEndpoint(String rawURI, String remaining, Map<String, Object> parameters) throws Exception {
-        TangoDevice proxy = new TangoDevice(DeviceProxyFactory.get("tango://" + remaining));
+        TangoDevice proxy = new TangoDevice(new DeviceProxy("tango://" + remaining));
 
         Endpoint endpoint = new TangoEndpoint(rawURI, this, proxy);
         setProperties(endpoint, parameters);
